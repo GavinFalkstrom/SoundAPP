0924f67772d946cda9da3456af7bb9bd9e9d127f
GavinFalkstrom <gavinfalkstrom@gmail.com>
2018-01-24 09:59:28 -0600

    hi

--- app/src/main/java/com/example/a599644/soundapp/MainActivity.java
+++ app/src/main/java/com/example/a599644/soundapp/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    MediaPlayer hi = MediaPlayer.create(getApplicationContext().R.raw.hi);
-    hi.start()
+
+
+    MediaPlayer hi = MediaPlayer.create(getApplicationContext(), R.raw.hi);
+    hi.start();
+}
 }
